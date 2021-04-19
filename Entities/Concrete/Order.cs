273840a5b9c28fbb4726a8b8fd09b07035e4c403
Gökhan SÖNMEZ <gsonmez828@aof.anadolu.edu.tr>
2021-04-19 23:15:53 +0300

    Add project files.

---
+++ Entities/Concrete/Order.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Order:IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int AddressId { get; set; }
+        public int OrderStatusId { get; set; }
+        public int Count { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ Entities/Concrete/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class OrderDetail:IEntity
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int PrdouctId { get; set; }
+        public int Count { get; set; }
+        public int SalePrice { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
